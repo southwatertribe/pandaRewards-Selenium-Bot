fd3f706a281e7ea323d62102860ec338f376878e
southwatertribe <94877162+southwatertribe@users.noreply.github.com>
2023-04-17 16:43:08 -0700

    New bot Updates
    There are now 2 lambda functions, one to create a task in DynamoDB. Then another one gets triggered by the first lambda with the task ID to run the survey. This allows for the application to poll for results preventing the Heroku timeout.

--- main.py
+++ main.py
@@ -1,3 +1,76 @@
+#survey_task lambda
+#Survey_task lambda that creates a temporary task in the database
+#Triggers panada_bot lambda
+try:
+    import json
+    import time
+    import os
+    import shutil
+    import uuid
+    import boto3
+    from datetime import datetime
+    import datetime
+
+    print("All Modules are ok ...")
+
+except Exception as e:
+
+    print("Error in Imports ")
+
+
+
+def store_task_status(task_id, status, table, result=None):
+    table.put_item(
+        Item={
+            'task_id': task_id,
+            'status': status,
+            'result': result
+        }
+    )
+
+def invoke_second_lambda(lambda_client, function_name, task_id, original_event):
+    payload = {
+        "task_id": task_id,
+        "original_event": original_event
+    }
+    response = lambda_client.invoke(
+        FunctionName=function_name,
+        InvocationType='Event',
+        Payload=json.dumps(payload)
+    )
+
+
+
+def lambda_handler(event, context):
+    
+     # TODO implement
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('survey_tasks')
+    task_id = str(uuid.uuid4())
+    store_task_status(task_id, 'pending', table)
+  
+  
+    lambda_client = boto3.client('lambda')
+    second_lambda_function_name = "PandaBot3_7"
+    invoke_second_lambda(lambda_client, second_lambda_function_name,task_id, event)
+    
+   
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({"task_id": task_id}),
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+        
+    }
+    return response
+
+
+
+
+
+#panda_bot lambda 
+#Bot that gets triggered by the survey lambda function 
 try:
     import json
     import time
@@ -37,25 +110,41 @@ class WebDriver(object):
         driver = Chrome('/opt/chromedriver', options=self.options)
         return driver
 
+def update_task_status(task_id, status, result):
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('survey_tasks')
+    table.update_item(
+        Key={'task_id': task_id},
+        UpdateExpression='SET #status = :status, #result = :result',
+        ExpressionAttributeNames={
+            '#status': 'status',
+            '#result': 'result',
+        },
+        ExpressionAttributeValues={
+            ':status': status,
+            ':result': result,
+        }
+    )
 
 
 def lambda_handler(event, context):
     
     #Initialize default message
-    message = "Sucess"
+    message = "success"
     
     #Instantiate driver and go to panda survey
     instance_ = WebDriver()
     driver = instance_.get()
     driver.get("https://www.pandaguestexperience.com/")
-    print(driver.page_source)
+    
     
     #CREATE DICTS
-    object = event['body']
-    objectDict = json.loads(object)
-    mainDict = objectDict["entry"]
+    objectDict = json.loads(event['original_event']['body'])
+    mainDict = objectDict['entry']
+    the_id = event['task_id']
     codeDict = mainDict["code"]
-    
+    print(codeDict.keys())
     #Create code List
     codeList = []
     for key, val in codeDict.items():
@@ -84,10 +173,14 @@ def lambda_handler(event, context):
          validate = driver.find_element_by_xpath('//*[@id="errorCN5"]').text
          #validate if it worked then break out if not  
          if validate == "* Error: Please answer this question.":
+            print("WRONG CODE BABYYY")
             message = "Wrong Code!"
+            status = "success"
+            result = message
+            update_task_status(the_id,status,result)
          return json.dumps(message)
     except NoSuchElementException:
-         message = "Sucess"
+         message = "success"
     
     #Codes validated do the survey
     #Begin surver button
@@ -131,6 +224,10 @@ def lambda_handler(event, context):
         if len(link) == 0:
             break
         link[0].click()
-            
+        
+    status = 'success'
+    result = message
+    #Update tasks
+    update_task_status(the_id,status,result)
         
     return json.dumps(message)
